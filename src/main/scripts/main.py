2048038730f5fde8f6b406e668b86fe7ac8c8cb4
minasonbol@gmail.com <minasonbol@gmail.com>
2020-11-10 15:04:24 -0500

    Revert "loading twitter handles"
    This reverts commit 8609d8fd5a266510176c8e855d8101fec8bc6681.

--- src/main/scripts/main.py
+++ src/main/scripts/main.py
@@ -1,8 +1,3 @@
-import json
-
-with open('docs/TwitterHandles/twitter_handles.json') as json_file:
-    twitter_handles = json.load(json_file)
-
 ### Instantiate the NBAPlayer objects ###
 
 # players = []
