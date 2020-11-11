0dfb183c43192398239daee34ade8d7e9599f97f
darkhark <joshua.harkness32@gmail.com>
2020-11-10 21:20:40 -0500

    TwitterDataCleaner.py now removes hashtags and mentions. If a tweet becomes an empty space after cleaning, that entire row is removed from the DF. Left some example code at the bottom in case testing is needed.

--- src/main/python/cleansers/TwitterDataCleaner.py
+++ src/main/python/cleansers/TwitterDataCleaner.py
@@ -1,9 +1,10 @@
 from pandas import DataFrame
 from nltk.corpus import stopwords
-
+import numpy as np
 import re
 # import nltk
 # nltk.download('stopwords')  # Uncomment if needed
+import loaders.TwitterDataLoader as tdl
 
 emoji_regrex_pattern = re.compile(pattern="["
                                           u"\U0001F600-\U0001F64F"  # emoticons
@@ -26,14 +27,20 @@ def cleanTweets(df: DataFrame.__class__) -> DataFrame.__class__:
     stopWords = stopwords.words('english')
     stopwordsRegexPattern = r'\b(?:{})\b'.format('|'.join(stopWords))
     cleanDF = df
-    # removes URLs, then punctuation, then emojis, converts everything to lowercase,
-    # removes stopping words, then removes excess spacing. The order is important.
-    cleanDF['tweet'] = df['tweet'].map(lambda tweet: re.sub(r"http\S+", "", tweet)) \
+    # removes URLs, then mentions, then hashtags, then punctuation,
+    # then emojis, converts everything to lowercase,
+    # removes stopping words, then removes excess spacing.
+    # Lastly, empty tweets are removed. The order is important.
+    cleanDF['tweet'] = df['tweet'].map(lambda tweet: re.sub(r'http\S+', '', tweet)) \
+        .map(lambda tweet: re.sub(r'@[A-Za-z0-9_]+', '', tweet)) \
+        .map(lambda tweet: re.sub(r'#[A-Za-z0-9_]+', '', tweet)) \
         .map(lambda tweet: re.sub(r'[^a-zA-z0-9\s]', '', tweet)) \
         .map(lambda tweet: emoji_regrex_pattern.sub(r'', tweet)) \
         .map(lambda tweet: tweet.lower()) \
         .map(lambda tweet: re.sub(stopwordsRegexPattern, '', tweet)) \
         .map(lambda tweet: removeExcessiveSpace(tweet))
+    cleanDF.apply(lambda x: np.nan if isinstance(x, str) and (x.isspace() or not x) else x)
+    cleanDF.dropna(subset=['tweet'], inplace=True)
     return cleanDF
 
 
@@ -41,3 +48,8 @@ def removeExcessiveSpace(tweet):
     while "  " in tweet:
         tweet = tweet.replace("  ", " ")
     return tweet
+
+# tweetsDF = tdl.getPlayerTweetsAsDF('KingJames', '2018-10-12', '2019-04-10')
+# print(tweetsDF)
+# tweetsDF = cleanTweets(tweetsDF)
+# print(tweetsDF)
