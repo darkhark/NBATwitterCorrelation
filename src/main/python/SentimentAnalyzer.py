a7d0ede715657446d0459c3a9b9f6dd85af77e41
afandino <afandino@users.noreply.github.com>
2020-11-15 12:57:46 -0500

    working on fixes, stepping out for few minutes.

--- src/main/python/SentimentAnalyzer.py
+++ src/main/python/SentimentAnalyzer.py
@@ -6,7 +6,7 @@ from azure.core.credentials import AzureKeyCredential
 _key = "adc96f39ac2a4918a1f462b5bf8674b8"
 _endpoint = "https://nbatwittersentiment.cognitiveservices.azure.com/"
 
-def _authenticate(key:str,endpoint:str):
+def __authenticate(key:str, endpoint:str):
     # Authentication
     ta_credential = AzureKeyCredential(key)
     client = TextAnalyticsClient(
@@ -15,33 +15,42 @@ def _authenticate(key:str,endpoint:str):
         api_version='TextAnalyticsApiVersion.V3_0')
     return client
 
-def _formatresponse(responseDict) -> pd.DataFrame :
-    col_list = responseDict.keys()
-    cleanresponse = {'tweetID':responseDict.id,
-                    'positive':responseDict.confidence_scores.positive,
-                    'neutral':responseDict.confidence_scores.neutral,
-                    'negative':responseDict.confidence_scores.negative
+def __formatresponse(responseList) -> pd.DataFrame:
+    """
+    The return value for a single document can be a result or error object.
+    A heterogeneous list containing a collection of result and error objects is returned
+    from each operation. These results/errors are index-matched with the order of the provided documents
+    """
+    col_list = responseList.keys()
+    cleanresponse = {'tweetID':responseList.id,
+                    'positive':responseList.confidence_scores.positive,
+                    'neutral':responseList.confidence_scores.neutral,
+                    'negative':responseList.confidence_scores.negative
                     }
-    for idx, sentence in enumerate(responseDict.sentences):
+    for idx, sentence in enumerate(responseList.sentences):
         cleanresponse ['tweet']= sentence.text
-    # sentiment_df = pd.DataFrame.from_dict(cleanresponse,orient='index')
     sentiment_df = pd.DataFrame.from_records(cleanresponse,index=[0])
     return sentiment_df
 
 def sentiment_analysis(tweet) -> pd.DataFrame:
     """Uses Microsoft Cognitive Service Text Analytics 3.0 to analyze text
-    @param tweet[] list: a list of dictionary tweet objects
+    @param tweet[] list:
     @return: A dataframe containing columns negative, neutral, positive,tweet,tweetID
     @rtype: Dataframe
-    usage:
-    tweet = {
-      "id": "1",
-     "text": "I am scoring some points in tonight's game"}
-    tweets = [tweet]
-    tweetAnalyzer = sentiment_analysis(tweets)
     """
-    client = _authenticate(_key,_endpoint)
-    response = client.analyze_sentiment(documents=tweet)[0]
-    tweetdf =_formatresponse(response)
+    if 'tweetID' in tweet.keys():
+        tweets = [tweet]
+        client = __authenticate(_key, _endpoint)
+        response = client.analyze_sentiment(documents=tweets)[0]
+        tweetdf = __formatresponse(response)
+    else:
+        print('tweetID is not present')
+        tweetdf = pd.DataFrame()
     return tweetdf
 
+# Test code
+tweet = {
+    "tweetID": "1",
+    "text": "I am scoring some points in tonight's game"}
+tweetAnalyzer = sentiment_analysis(tweet)
+print(tweetAnalyzer)
