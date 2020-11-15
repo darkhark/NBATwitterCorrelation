66c521aa071f89161c52b2f3e90926e6e82ad192
afandino <afandino@users.noreply.github.com>
2020-11-14 22:07:25 -0500

    this file will be replaced

--- src/main/python/SentimentAnalyzer.py
+++ src/main/python/SentimentAnalyzer.py
@@ -1,40 +1,46 @@
+import pandas as pd
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 
-def sentiment_analysis(tweet):
+_key = "adc96f39ac2a4918a1f462b5bf8674b8"
+_endpoint = "https://nbatwittersentiment.cognitiveservices.azure.com/"
+
+def _authenticate(key:str,endpoint:str):
     # Authentication
-    key = "adc96f39ac2a4918a1f462b5bf8674b8"
-    endpoint = "https://nbatwittersentiment.cognitiveservices.azure.com/"
     ta_credential = AzureKeyCredential(key)
     client = TextAnalyticsClient(
         endpoint=endpoint,
         credential=ta_credential,
         api_version='TextAnalyticsApiVersion.V3_0')
+    return client
+
+def _formatresponse(responseDict) -> pd.DataFrame :
+    col_list = responseDict.keys()
+    cleanresponse = {'tweetID':responseDict.id,
+                    'positive':responseDict.confidence_scores.positive,
+                    'neutral':responseDict.confidence_scores.neutral,
+                    'negative':responseDict.confidence_scores.negative
+                    }
+    for idx, sentence in enumerate(responseDict.sentences):
+        cleanresponse ['tweet']= sentence.text
+    # sentiment_df = pd.DataFrame.from_dict(cleanresponse,orient='index')
+    sentiment_df = pd.DataFrame.from_records(cleanresponse,index=[0])
+    return sentiment_df
+
+def sentiment_analysis(tweet) -> pd.DataFrame:
+    """Uses Microsoft Cognitive Service Text Analytics 3.0 to analyze text
+    @param tweet[] list: a list of dictionary tweet objects
+    @return: A dataframe containing columns negative, neutral, positive,tweet,tweetID
+    @rtype: Dataframe
+    usage:
+    tweet = {
+      "id": "1",
+     "text": "I am scoring some points in tonight's game"}
+    tweets = [tweet]
+    tweetAnalyzer = sentiment_analysis(tweets)
+    """
+    client = _authenticate(_key,_endpoint)
     response = client.analyze_sentiment(documents=tweet)[0]
-    return response.sentences
-    # print("Document Sentiment: {}".format(response.sentiment))
-    # print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
-    #     response.confidence_scores.positive,
-    #     response.confidence_scores.neutral,
-    #     response.confidence_scores.negative,
-    # ))
-    # for idx, sentence in enumerate(response.sentences):
-    #     print("Sentence: {}".format(sentence.text))
-    #     print("Sentence {} sentiment: {}".format(idx + 1, sentence.sentiment))
-    #     print("Sentence score:\nPositive={0:.2f}\nNeutral={1:.2f}\nNegative={2:.2f}\n".format(
-    #         sentence.confidence_scores.positive,
-    #         sentence.confidence_scores.neutral,
-    #         sentence.confidence_scores.negative,
-    #     ))
+    tweetdf =_formatresponse(response)
+    return tweetdf
 
-
-tweet1 = {
-    "language": "en",
-    "id": "1",
-    "text": "Hello world This is some input text that I love."
-}
-
-tweets = [tweet1]
-tweetAnalyzer = sentiment_analysis(tweets)
-for x in tweetAnalyzer:
-    print(x)
